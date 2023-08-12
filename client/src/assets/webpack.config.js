d291f507c55b16ef9a87d892f8efd246c03fadf4
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-08-12 12:50:41 +0300

    Delete client directory

--- client/src/assets/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/assets/js/contentful.js', // Entry point of your application
-  output: {
-    filename: 'bundle.js', // Output file name
-    path: path.resolve(__dirname, 'dist') // Output directory
-  }
-};
--- client/src/types/postInterface.ts
+++
@@ -1,7 +0,0 @@
-export interface Post {
-    title: string,
-    image: string,
-    tags: string,
-    description: string
-
-}
